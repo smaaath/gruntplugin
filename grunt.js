0e5457ef3de9a9d6325bcddf5a33c85f23f020dc
smaaath <schmitt.mathieu@gmail.com>
2013-04-04 16:36:02 +0200

    update grunt.js of plugin

--- grunt.js
+++ grunt.js
@@ -1,26 +1,31 @@
-module.exports = function (grunt) {
+/*
+ * test
+ * https://github.com/gruntjs/grunt-init-node-sample
+ *
+ * Copyright (c) 2013 smaaath
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+
     // Project configuration.
 
+    /*  console.log('GRUNTGRUNT.config.get("moduleTests").sourcesDirectory',grunt.config.get("moduleTests").sourcesDirectory);
+     // Actually load this plugin's task(s).
+     grunt.loadTasks('tasks');
+
+     // These plugins provide necessary tasks.
+     grunt.loadNpmTasks('grunt-contrib-jshint');
+     grunt.loadNpmTasks('grunt-contrib-clean');
 
 
-    //grunt.loadTasks('node_modules/grunt-tasks/tasks');
-    grunt.loadNpmTasks('grunt-tasks');
+     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+     // plugin's task(s), then test the result.
+     grunt.registerTask('test', ['clean', 'test']);
+     // By default, lint and run all tests.
+     grunt.registerTask('default', ['lint', 'test']);*/
 
-    grunt.loadTasks('node_modules/grunt-tasks/tasks');
-
-    // These plugins provide necessary tasks.
-
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    //
-    // console.log('grunt.config.get("smTests").sourcesDirectory',grunt.config.get("smTests").sourcesDirectory);
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'test', 'nodeunit']);
-
-    // By default, lint and run all tests.
-    grunt.registerTask('default', ['jshint', 'test']);
-    // Default task.
-    //grunt.registerTask('default', 'lint test');
 };
-
